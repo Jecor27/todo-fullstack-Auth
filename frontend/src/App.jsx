97ba7d297300dc6e71cf0da7f7833643a2e421c7
jadedrb <jadecdrb@gmail.com>
2025-03-05 11:51:45 -0500

    initial request made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,17 @@
+import { useEffect } from 'react'
 import './App.css'
 
 function App() {
+
+  async function test() {
+    const response = await fetch('http://localhost:8080/test')
+    const result = await response.json()
+    console.log(result)
+  }
+
+  useEffect(() => {
+    test()
+  }, [])
 
   return (
     <>

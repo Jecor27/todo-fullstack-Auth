d7142f303d8a86b79eb2b55bf59c1b45e620c90e
oussamahamani <oussamahamani2000@gmail.com>
2025-03-06 13:24:28 -0500

    tracking loading error and other stuff

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -4,22 +4,38 @@ const LogIn = () => {
     const [email,setEmail] = useState('')
     const [password,setPassword] = useState('')
 
+    const [loading,setLoading]=useState(false)
+    const [success,setSuccess]=useState(false)
+    const [error,setError]= useState("")
     const handleSubmit = async(e)=>{
         e.preventDefault()
+        if(loading) {
+            console.log("a request is happening")
+            return
+        }
+        console.log("new request")
+        setLoading(true)
+        setSuccess(false)
+        setError("")
         let data={
             email,
            password
         }
         let url = import.meta.env.VITE_API // http://localhost:8080
-
+        
         try {
-            
+            await new Promise(resolve => setTimeout(resolve, 2000));
+
             let response = await axios.post(url+"/api/login",data)
-             console.log(response)
-        } catch (error) {
-            console.log("🚀 ~ handleSubmit ~ error:", error)
             
-        }
+            console.log(response)
+            setSuccess(true)
+            } catch (error) {
+                console.log("🚀 ~ handleSubmit ~ error:", error)
+                setError(error.response.data.error)
+                
+            }
+            setLoading(false)
     }
     return (
         <div>
@@ -34,9 +50,12 @@ const LogIn = () => {
 <br/>
             <label htmlFor="">
                 password
-                <input type="text" name="" id="" onChange={(e)=>setPassword(e.target.value)}/>
+                <input  type="text" name="" id="" onChange={(e)=>setPassword(e.target.value)}/>
             </label>
             <button >Log In</button>
+            {loading&&<h5>loading</h5>}
+            {error&&<h5>{error}</h5>}
+            {success &&<h5>You are in</h5>}
           </form>
         </div>
     );

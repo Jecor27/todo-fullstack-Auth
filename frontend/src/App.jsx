20f105f0414e9283b8d8235ad44b7ad3b39c653b
oussamahamani <oussamahamani2000@gmail.com>
2025-03-06 11:12:58 -0500

    moving the content of app into todo component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,84 +1,11 @@
-import { useEffect, useState, useRef } from 'react'
-import './App.css'
+import React from 'react';
 
-const BASE_URL = import.meta.env.VITE_API
-
-function App() {
-
-  const [todos, setTodos] = useState([])
-  const textRef = useRef()
-
-  async function getTodos() {
-    const response = await fetch(`${BASE_URL}/api/todos`)
-    const result = await response.json()
-    setTodos(result)
-  }
-
-  useEffect(() => {
-    getTodos()
-  }, [])
-
-  async function handleSubmit(e) {
-    e.preventDefault()
-    const todo = { 
-      text: textRef.current.value
-    }
-    const response = await fetch(`${BASE_URL}/api/todos`, {
-      method: 'POST',
-      body: JSON.stringify(todo),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    const todoDoc = await response.json()
-    setTodos([...todos, todoDoc])
-    textRef.current.value = ''
-  }
-
-  async function handleDelete(id) {
-    await fetch(`${BASE_URL}/api/todos/${id}`, {
-      method: 'DELETE'
-    })
-    getTodos()
-  }
-
-  async function handleComplete(id) {
-    const foundTodo = todos.find(todo => todo._id == id)
-    foundTodo.completed = !foundTodo.completed
-    await fetch(`${BASE_URL}/api/todos/${id}`, {
-      method: 'PUT',
-      body: JSON.stringify(foundTodo),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    getTodos()
-  }
-
-  console.log(todos)
-
-  return (
-    <>
-      <h1>Todos</h1>
-      <form onSubmit={handleSubmit}>
-        <input ref={textRef} />
-        <button>Submit</button>
-      </form>
-      <ul>
-        {todos.map(todo => 
-          <li key={todo._id}>
-            <input 
-              type="checkbox" 
-              checked={todo.completed} 
-              onChange={() => handleComplete(todo._id)}
-            />
-            {todo.text}
-            <button onClick={() => handleDelete(todo._id)}>X</button>
-          </li>
-        )}
-      </ul>
-    </>
-  )
+const App = () => {
+    return (
+        <div>
+            <h1>hello world</h1>
+        </div>
+    );
 }
 
-export default App
+export default App;
